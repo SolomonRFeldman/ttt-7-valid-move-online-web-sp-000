3c1ff5187509296c1821d6b2058b46af7d3b7860
affable-motherboard-2531 <solomonrfeldman@gmail.com>
2019-03-20 14:40:00 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
-def valid_move?(index)
-  index.between(0,8) && !position_taken
+def valid_move?(board, index)
+  board[index].between(0,8) && !position_taken
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
